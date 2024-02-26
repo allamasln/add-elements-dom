d1eeae0875e1223173431d8172870654cb167875
Antonio Llamas <antoniollamasln@gmail.com>
2024-02-26 13:49:09 +0100

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
- // Aquí tu código
+const listElDOM = document.getElementById('lista')
+const buttonElDOM = document.getElementById('agregar')
+
+buttonElDOM.addEventListener('click', function () {
+	const newItem = prompt('Añade un nuevo elemento a la lista:')
+
+	// listElDOM.innerHTML += `<li>${newItem}</li>`
+
+	const itemEl = document.createElement('li')
+	itemEl.textContent = newItem
+
+	listElDOM.appendChild(itemEl)
+})
